871d3659d26bc09557da0ef1fbaab33fc7416604
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2023-11-30 14:05:23 +0100

    update spawn lcoation

--- src/new_game.c
+++ src/new_game.c
@@ -126,7 +126,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(MAP_NATIONAL_HISTORY_MUSEUM_L3), MAP_NUM(MAP_NATIONAL_HISTORY_MUSEUM_L3), WARP_ID_NONE, 27, 8);
     WarpIntoMap();
 }
 
