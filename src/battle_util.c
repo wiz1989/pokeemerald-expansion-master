3b33d031e2709b5f2fe859168cd68f00ee1bf551
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2024-01-29 18:07:51 +0100

    Revert "Remove redundant AI score increases (#3504)"
    This reverts commit a21e77d55dd7d084279387807b8c385667a00def.

--- src/battle_util.c
+++ src/battle_util.c
@@ -8707,7 +8707,7 @@ static inline u32 CalcMoveBasePower(u32 move, u32 battlerAtk, u32 battlerDef, u3
         if (gBattleMons[battlerDef].status1 & STATUS1_SLEEP || abilityDef == ABILITY_COMATOSE)
             basePower *= 2;
         break;
-    case EFFECT_SMELLING_SALTS:
+    case EFFECT_SMELLINGSALT:
         if (gBattleMons[battlerDef].status1 & STATUS1_PARALYSIS)
             basePower *= 2;
         break;
