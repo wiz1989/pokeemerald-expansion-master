9d5019e7520d70cbfd606723b8613a1380644e7e
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2024-01-29 18:08:00 +0100

    Revert "Use move flags instead of lists for AI calcs (#3389)"
    This reverts commit 72296fab02971a28b3047e3c237ba499d8d26efb.

--- src/battle_ai_main.c
+++ src/battle_ai_main.c
@@ -2534,7 +2534,8 @@ static s32 AI_CheckBadMove(u32 battlerAtk, u32 battlerDef, u32 move, s32 score)
 
                 if (instructedMove == MOVE_NONE
                   || gBattleMoves[instructedMove].instructBanned
-                  || gBattleMoves[instructedMove].effect == EFFECT_RECHARGE
+                  || MoveRequiresRecharging(instructedMove)
+                  || MoveCallsOtherMove(instructedMove)
                   || IsZMove(instructedMove)
                   || (gLockedMoves[battlerDef] != 0 && gLockedMoves[battlerDef] != 0xFFFF)
                   || gBattleMons[battlerDef].status2 & STATUS2_MULTIPLETURNS
