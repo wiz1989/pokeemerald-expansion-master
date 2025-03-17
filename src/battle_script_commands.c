1479ae799d66fb452aedaf286e36827fc0e9181a
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2025-03-17 12:17:31 +0100

    Update src/battle_script_commands.c
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -18650,7 +18650,7 @@ void BS_JumpIfCanGigantamax(void)
     NATIVE_ARGS(u8 battler, const u8 *jumpInstr);
     u32 battler = GetBattlerForBattleScript(cmd->battler);
 
-    if (PartyMonHasGigantamaxFactor(gBattlerPartyIndexes[battler])
+    if (GetMonData(&gPlayerParty[gBattlerPartyIndexes[battler]], MON_DATA_GIGANTAMAX_FACTOR))
       && GetGMaxTargetSpecies(gBattleMons[battler].species) != SPECIES_NONE)
         gBattlescriptCurrInstr = cmd->jumpInstr;
     else
