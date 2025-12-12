5b20ee15204f1164497525f0f3f8eb021877d74b
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2025-12-12 12:03:04 +0100

    increased bag size

--- include/constants/global.h
+++ include/constants/global.h
@@ -77,9 +77,9 @@
 #define ROAMER_COUNT 1 // Number of maximum concurrent active roamers
 
 // Bag constants
-#define BAG_ITEMS_COUNT 30
+#define BAG_ITEMS_COUNT 90
 #define BAG_KEYITEMS_COUNT 30
-#define BAG_POKEBALLS_COUNT 16
+#define BAG_POKEBALLS_COUNT 30
 #define BAG_TMHM_COUNT 64
 #define BAG_BERRIES_COUNT 46
 
