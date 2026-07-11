984df1df423267229820d7f6e1d018d6e2647780
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2026-07-11 21:32:37 +0200

    updated poryscript and added a first test script

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -51,8 +51,8 @@
 
 #else
 
-#define FLAG_SMART_WILD_MONS    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
+#define FLAG_SMART_WILD_MONS 0x20 // Unused Flag
+#define FLAG_BASE_SUNKERN    0x21 // Unused Flag
 #define FLAG_UNUSED_0x022    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
