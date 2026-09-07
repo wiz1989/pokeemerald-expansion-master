a965560d8369096ea4986e07d5cb019b54956bd4
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2026-09-07 23:33:45 +0200

    some fixes

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -101,7 +101,7 @@ enum __attribute__((packed)) Stat
 #define MAX_STAT_STAGE    12
 
 // Shiny odds
-#define SHINY_ODDS 8 // Actual probability is SHINY_ODDS/65536
+#define SHINY_ODDS 512 // Actual probability is SHINY_ODDS/65536
 
 // Ribbon IDs used by TV and Pokénav
 #define CHAMPION_RIBBON       0
